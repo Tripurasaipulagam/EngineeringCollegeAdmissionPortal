2f655c285a1f42bc30f4a4e3e16ac6ccb01dda8b
Tripura Sai <pulagamtripurasai@gmail.com>
2026-02-14 12:51:31 +0530

    Add files via upload

---
+++ Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EngineeringCollegeAPI.Models
+{
+    public class Course
+    {
+       
+        public int CourseId { get; set; }
+
+        [Required]
+        public string CourseName { get; set; }
+
+        
+    }
+}
